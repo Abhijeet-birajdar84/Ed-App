e2713ac999080259a8e3d5603e9b5c58c407b787
Abhijeet-birajdar84 <abhjeetbirajdar90@gmail.com>
2025-04-19 23:38:03 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
@@ -50,8 +50,6 @@ export const clerkWebhooks = async (req, res) => {
         }
         
     } catch (error) {
-        console.error('Function error:', error);
-        res.status(500).json({ message: 'Internal Server Error', error: error.message });
-        
+       res.json({success: false, message: error.message})        
     }
 }
